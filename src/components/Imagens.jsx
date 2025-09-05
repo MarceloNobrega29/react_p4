64c537ee903b79d9d2a14e1b06adb93e394a53b3
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-09-05 11:31:59 -0300

    Exercicio Aula 02

---
+++ src/components/Imagens.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import r9 from '../assets/r9.jpg'
+import balotteli from '../assets/balotteli.jpg'
+
+const Imagens = () => {
+  return (
+    <div>
+        <img src={r9} alt="r9 foto" />
+        <img src={balotteli} alt="balotteli foto" />
+    </div>
+  )
+}
+
+export default Imagens
